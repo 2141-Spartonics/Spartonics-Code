e596aac6d4250b4708b0ba35537ca85f1c55e6d1
Bernstern <legomaker@Me.com>
2017-01-22 15:30:00 -0800

    Training

--- 2017 Code/src/org/usfirst/frc/team2141/robot/Robot.java
+++ 2017 Code/src/org/usfirst/frc/team2141/robot/Robot.java
@@ -27,7 +27,7 @@ public class Robot extends IterativeRobot {
 
 	Command autonomousCommand;
 	SendableChooser chooser;
-
+//test
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
