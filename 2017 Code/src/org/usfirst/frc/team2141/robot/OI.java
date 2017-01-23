4c66076e9529391c9c8ca36939d806ac5d9d9ae9
Bernstern <legomaker@Me.com>
2017-01-23 15:26:13 -0800

    Removing extra commands and subsystems

--- 2017 Code/src/org/usfirst/frc/team2141/robot/OI.java
+++ 2017 Code/src/org/usfirst/frc/team2141/robot/OI.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team2141.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
-import org.usfirst.frc.team2141.robot.commands.ExampleCommand;
 
 /**
  * This class is the glue that binds the controls on the physical operator
