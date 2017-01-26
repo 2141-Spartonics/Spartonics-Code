7beb53ad4d242b508882bc77357f39c9a74e1ea4
Justi <Justi@10.2.1.48>
2017-01-26 14:27:52 -0800

    Made the execute for intake command

--- 2017 Code/src/org/usfirst/frc/team2141/robot/commands/IntakeCommand.java
+++ 2017 Code/src/org/usfirst/frc/team2141/robot/commands/IntakeCommand.java
@@ -21,6 +21,7 @@ public class IntakeCommand extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	Robot.intake.setIntakeMotor(.8);
     }
 
     // Make this return true when this Command no longer needs to run execute()
