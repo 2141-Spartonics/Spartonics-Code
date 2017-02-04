47f0f30850a25f99d6a6a8fbe0a3aef6cb7dbc0f
JustinDew <Justinluu67@yahoo.com>
2017-02-03 18:32:31 -0800

    Put in the voltage controlMode -shooter

--- 2017 Code/src/org/usfirst/frc/team2141/robot/subsystems/Shooter.java
+++ 2017 Code/src/org/usfirst/frc/team2141/robot/subsystems/Shooter.java
@@ -13,48 +13,27 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Shooter extends Subsystem {
-<<<<<<< Upstream, based on master
-	
+
 	private CANTalon shooterMotor;
-=======
-	CANTalon shooterMotor;
-	CANTalon hopperMotor;
->>>>>>> 3d7535d Added hopper code to Shooter subsystem and updated Robotmap
-	Encoder shooterEncoder;
-	PIDController shooterPID;
-	PIDOutput output;
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-    public Shooter(){
-    	shooterMotor = new CANTalon(RobotMap.SHOOTER_MOTOR);
-    	hopperMotor = new CANTalon(RobotMap.HOPPER_MOTOR);
-    	output = new PIDOutput() {
-			
-  
-			@Override
-			public void pidWrite(double output) { 
-				// TODO Auto-generated method stub
-				
-			}
-		};
-    	shooterPID = new PIDController(0, 0, 0, this.shooterEncoder, output);
-    }
-   
-    public void setShooterMotor(double speed){
-    	this.shooterMotor.set(speed);
-    }
-    
-    public void setHopperMotor(double speed){
-    	this.hopperMotor.set(speed);
-    }
-    
-    public double getVelocity(){
-    	return this.shooterEncoder.getRate();
-    }
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+
+	public Shooter() {
+		shooterMotor = new CANTalon(RobotMap.SHOOTER_MOTOR);
+
+	}
+
+	public void setShooterMotor(double speed) {
+		this.shooterMotor.set(speed);
+	}
+
+	public double getVelocity() {
+		return this.shooterMotor.getSpeed();
+	}
 }
-
