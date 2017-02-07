23887acb306402737aaeb1aeee084c97eef0caf1
Alex Colello <22144@dlshs.org>
2017-02-06 20:04:35 -0800

    Cleaned up after commit screw up

--- 2017 Code/src/org/usfirst/frc/team2141/robot/commands/FlipChassisDirection.java
+++ 2017 Code/src/org/usfirst/frc/team2141/robot/commands/FlipChassisDirection.java
@@ -17,7 +17,7 @@ public class FlipChassisDirection extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	Robot.chassis.setBothToLowGear();
+    	Robot.chassis.setBothToLowSpeed();
     	Robot.chassis.setLeftMotors(0);
     	Robot.chassis.setRightMotors(0);
     	Robot.chassis.flipDirection();
