d364c42dbbf0f652fdd5d2f9ef16d30a50710408
Bernstern <legomaker@Me.com>
2017-02-10 18:24:04 -0800

    Alex's Edits to Drive Straight

--- 2017 Code/src/org/usfirst/frc/team2141/robot/commands/DriveStraight.java
+++ 2017 Code/src/org/usfirst/frc/team2141/robot/commands/DriveStraight.java
@@ -10,13 +10,11 @@ import edu.wpi.first.wpilibj.command.Command;
 public class DriveStraight extends Command {
 
 	double encoderCount;
-	double bernsConstant;
-
-	public DriveStraight() {
+	
+	public DriveStraight(double distance) {
 		// Use requires() here to declare subsystem dependencies
-		requires(Robot.chassis);
-		bernsConstant = 12/13;
-		
+		requires(Robot.chassis);		
+		encoderCount = distance;
 	}
 
 	// Called just before this Command runs the first time
@@ -25,28 +23,22 @@ public class DriveStraight extends Command {
 	}
 
 	// Called repeatedly when this Command is scheduled to run
-	protected void execute(double distance) {
+	protected void execute() {
 		Robot.chassis.setLeftMotorVoltage(.5);
 		Robot.chassis.setRightMotorVoltage(.5);
-		
-		encoderCount = distance / (12.56);
-		System.out.println("Encoder Count="+encoderCount);
-
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		if ((Math.abs(Robot.chassis.getRightEncoderCount()) + (Math.abs(Robot.chassis.getLeftEncoderCount()))) / 2 > encoderCount) {
-			return true;
+		if (encoderCount > 0) {
+			return Robot.chassis.getAverageEncoderPosition() > encoderCount;
 		} else {
-			return false;
+			return Robot.chassis.getAverageEncoderPosition() < encoderCount;
 		}
-
 	}
 
 	// Called once after isFinished returns true
 	protected void end() {
-		Robot.chassis.zeroEncoders();
 	}
 
 	// Called when another command which requires one or more of the same
