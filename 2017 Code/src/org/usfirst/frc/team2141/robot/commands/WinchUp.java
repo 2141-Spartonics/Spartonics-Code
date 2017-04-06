5c58fae345236efa7f1f8e6ed24e1aa7e90eec61
Bernstern <legomaker@Me.com>
2017-04-06 14:15:17 -0700

    Final post competition commit

--- 2017 Code/src/org/usfirst/frc/team2141/robot/commands/WinchUp.java
+++ 2017 Code/src/org/usfirst/frc/team2141/robot/commands/WinchUp.java
@@ -27,7 +27,7 @@ public class WinchUp extends Command {
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-		return Robot.winch.getCurrent() > 50;
+		return Robot.winch.getCurrent() > 60;
 	}
 
 	// Called once after isFinished returns true
