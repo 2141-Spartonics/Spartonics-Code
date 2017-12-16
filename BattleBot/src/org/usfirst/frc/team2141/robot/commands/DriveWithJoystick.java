ce9e38b616c95a904c339c0e88f7da89e6c0f1d7
legom <legomaker@Me.com>
2017-12-16 14:06:32 -0800

    Test

--- BattleBot/src/org/usfirst/frc/team2141/robot/commands/DriveWithJoystick.java
+++ BattleBot/src/org/usfirst/frc/team2141/robot/commands/DriveWithJoystick.java
@@ -21,9 +21,8 @@ public class DriveWithJoystick extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.chassis.driveWithJoystick(Robot.oi.getDriveStick());
-    	//Super important code
-    	    }
-
+    	//Super important code 
+    }
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
