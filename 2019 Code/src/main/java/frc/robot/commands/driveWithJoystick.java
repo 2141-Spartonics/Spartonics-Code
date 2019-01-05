d330a1448721b635a0af51be28d2356ca0c17322
Bernie Conrad <charlie.conrad16@gmail.com>
2019-01-05 15:37:50 -0800

    Update migration

--- 2019 Code/src/main/java/frc/robot/commands/driveWithJoystick.java
+++ 2019 Code/src/main/java/frc/robot/commands/driveWithJoystick.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,13 +10,11 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class driveWithJoystick extends Command {
+  public driveWithJoystick() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+    // eg. requires(chassis);
+    requires(Robot.chassis);
   }
 
   // Called just before this Command runs the first time
@@ -27,6 +25,7 @@ public class ExampleCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.chassis.arcadeDrive(Robot.oi.getRightY(), Robot.oi.getLeftX());
   }
 
   // Make this return true when this Command no longer needs to run execute()
