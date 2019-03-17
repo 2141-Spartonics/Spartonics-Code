9cc5cbb9ecebd3ab90fe8fb0c10ac45b82a7e2b2
Bernie Conrad <charlie.conrad16@gmail.com>
2019-03-17 11:18:04 -0700

    Update no 107

---
+++ 2019 Code/src/main/java/frc/robot/subsystems/ArduinoInterface.java
@@ -0,0 +1,62 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Add your docs here.
+ */
+public class ArduinoInterface extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public SerialPort arduino;
+
+  public ArduinoInterface() {
+    arduino = new SerialPort(9600, SerialPort.Port.kUSB);
+
+  }
+
+  public void setSerialPortBuffer(String message) {
+    arduino.writeString(message);
+  }
+
+  public String getSerialPortBuffer() {
+    System.out.println(arduino.readString());
+    return arduino.readString();
+  }
+
+  public void coordsRequest() {
+    setSerialPortBuffer("get");
+  }
+
+  /*
+
+  */
+  public double findAngle(String arduinoOutput) {
+    double angleFindX1 = Double.parseDouble((arduinoOutput.substring(9, 10)));
+    double angleFindY1 = Double.parseDouble(arduinoOutput.substring(12, 13));
+    double angleFindX2 = Double.parseDouble(arduinoOutput.substring(17, 18));
+    double angleFindY2 = Double.parseDouble(arduinoOutput.substring(20, 21));
+
+    double differenceX = Math.abs(angleFindX2 - angleFindX1);
+    double differenceY = Math.abs(angleFindY2 - angleFindY1);
+    double Angle = Math.toDegrees(Math.atan((differenceY) / (differenceX)));
+
+    return Angle;
+
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
